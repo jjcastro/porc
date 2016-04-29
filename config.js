d235b7a1623b3a89e915f7e0408cf3ae5e13389f
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-04-29 00:25:21 -0500

    Testing modulus database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-  'database': 'mongodb://localhost/node-api',
+  'database': 'mongodb://jjcastro:whatthehell7@jello.modulusmongo.net:27017/udabeR8y',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
