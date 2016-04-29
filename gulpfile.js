b4e0920d93efb01e433c1056a85f99d60f685c8e
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-04-29 11:19:21 -0500

    Gulp: Compile LESS and Minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 // load the plugins
-var gulp = require('gulp');
-var less = require('gulp-less');
+var gulp      = require('gulp');
+var less      = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var rename    = require('gulp-rename');
 
 // define a task called css
 gulp.task('css', function() {
@@ -8,6 +10,8 @@ gulp.task('css', function() {
   // grab the less file, process the LESS, save to style.css
   return gulp.src('public/assets/css/style.less')
     .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min'}))
     .pipe(gulp.dest('public/assets/css'));
-    
+
 });
