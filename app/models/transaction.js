e51f1140057360b768e76453e61c4b5ceaa16f56
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-05-07 20:02:24 -0500

    Agregar Transaction

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -1,12 +1,22 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-var bcrypt 		 = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+var bcrypt 	 = require('bcrypt-nodejs');
 
 // user schema 
-var TransactionSchema   = new Schema({
-	amount: String,
-	currency: "COP",
-	user: ObjectId
+var TransactionSchema = new Schema({
+  name:        { type: String, required: true },
+  description: String,
+
+  date:        { type: Date, default: Date.now }
+  _userId:     { type: Schema.Types.ObjectId, required: true },
+  
+  amount: { 
+    isExpense: Boolean,
+    value:     { type: Number, required: true },
+    currency:  { type: String, default: "COP" }
+  },
+
+  tags: [{ name: String, color: String }]
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
