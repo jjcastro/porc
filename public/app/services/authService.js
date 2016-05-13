33eb094c09a0f0eb535fb68d5a06d27855b23f2c
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-05-13 17:33:02 -0500

    Fix AuthService

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -15,7 +15,7 @@ angular.module('authService', [])
 	authFactory.login = function(username, password) {
 
 		// return the promise object and its data
-		return $http.post('/api/authenticate', {
+		return $http.post('/api/auth', {
 			username: username,
 			password: password
 		})
