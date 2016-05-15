7d9fefb2d3d656508225945e0faca371a969a6db
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-05-15 00:58:16 -0500

    Use ui-router instead of ng-route

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,43 +1,47 @@
-angular.module('app.routes', ['ngRoute'])
+angular.module('app.routes', ['ui.router'])
 
-.config(function($routeProvider, $locationProvider) {
-
-	$routeProvider
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise("/");
+  $locationProvider.html5Mode(true);
+	$stateProvider
 
 		// route for the home page
-		.when('/', {
+		.state('home', {
+      url: '/',
 			templateUrl : 'app/views/pages/home.html'
 		})
 		
 		// login page
-		.when('/login', {
-			templateUrl : 'app/views/pages/login.html',
+		.state('login', {
+      url: '/login',
+			templateUrl : 'app/views/app/login.html',
  			controller  : 'mainController',
 			controllerAs: 'login'
 		})
 		
 		// show all users
-		.when('/overview', {
-			templateUrl: 'app/views/pages/overview/main.html',
+		.state('overview', {
+      url: '/overview',
+			templateUrl: 'app/views/app/overview/main.html',
 			controller: 'overviewController',
 			controllerAs: 'overview'
 		})
 
 		// form to create a new transaction
 		// same view as edit page
-		.when('/transactions/create', {
-			templateUrl: 'app/views/pages/transactions/single.html',
+		.state('createtransaction', {
+      url: '/transactions/create',
+			templateUrl: 'app/views/app/transactions/single.html',
 			controller: 'transactionCreateController',
 			controllerAs: 'transaction'
 		})
 
 		// page to edit a transaction
-		.when('/transactions/:tran_id', {
-			templateUrl: 'app/views/pages/transactions/single.html',
+		.state('edittransaction', {
+      url: '/transactions/:tran_id',
+			templateUrl: 'app/views/app/transactions/single.html',
 			controller: 'transactionEditController',
 			controllerAs: 'transaction'
 		});
-
-	$locationProvider.html5Mode(true);
 
 });
